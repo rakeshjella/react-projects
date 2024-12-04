c3c5639709a47eda78540e8df1a3feae87b89484
rakeshjella <rakeshjella123@gmail.com>
2024-12-04 20:26:57 +0530

    fix: counter changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,30 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  function IncrementCounter() {
+    return setCount(count+1)
+  }
+  function DecrementCounter() {
+    if(count > 0) {
+      return setCount(count-1)
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
+      <h3> count is  {count}</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        
+        <button onClick={IncrementCounter}>
+         Increment
+        </button>
+        <button onClick={DecrementCounter}>
+          Decrement
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
